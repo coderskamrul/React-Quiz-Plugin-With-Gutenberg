34a1d9cf91b2bc0fa13e9d2344966a3a616edd0c
coderskamrul <mdkamrul2058@gmail.com>
2025-05-11 23:34:17 +0600

    Add Quiz Menu With Dashboard Submenu

--- includes/admin/class-react-quiz-app-menu.php
+++ includes/admin/class-react-quiz-app-menu.php
@@ -2,3 +2,46 @@
 /**
  * Admin menu class
  */
+class React_Quiz_App_Menu {
+
+	/**
+	 * Initialize the admin menu
+	 */
+	public function init() {
+		add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+	}
+
+	/**
+	 * Add admin menu
+	 */
+	public function add_admin_menu() {
+		add_menu_page(
+			__( 'Quiz App', 'react-quiz-app' ),
+			__( 'Quiz App', 'react-quiz-app' ),
+			'manage_options',
+			'react-quiz-app',
+			array( $this, 'render_admin_page' ),
+			'dashicons-list-view',
+			30
+		);
+
+		add_submenu_page(
+			'react-quiz-app',
+			__( 'Dashboard', 'react-quiz-app' ),
+			__( 'Dashboard', 'react-quiz-app' ),
+			'manage_options',
+			'react-quiz-app',
+			array( $this, 'render_admin_page' )
+		);
+	}
+
+	/**
+	 * Render admin page
+	 */
+	public function render_admin_page() {
+		echo '<div id="react-quiz-app-admin"></div>';
+	}
+
+
+
+}
