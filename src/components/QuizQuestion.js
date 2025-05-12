e46eb59aadab1e7e8b87eda61b38f51e3e156544
coderskamrul <mdkamrul2058@gmail.com>
2025-05-12 22:49:07 +0600

    Render all Quiz Question List

--- src/components/QuizQuestion.js
+++ src/components/QuizQuestion.js
@@ -1,6 +1,11 @@
+import { useQuiz } from "../contexts/QuizContext"
 
 const QuizQuestion = ({ question, questionIndex, userAnswer }) => {
+  const { setUserAnswer } = useQuiz()
 
+  const handleAnswerSelect = (answerId) => {
+    setUserAnswer(questionIndex, answerId)
+  }
 
   if (!question) {
     return null
@@ -10,6 +15,25 @@ const QuizQuestion = ({ question, questionIndex, userAnswer }) => {
     <div className="quiz-question">
       <h3 className="question-text">{question.text}</h3>
 
+      <div className="question-answers">
+        {question.answers.map((answer, index) => (
+          <div
+            key={index}
+            className={`answer-option ${userAnswer === index ? "selected" : ""}`}
+            onClick={() => handleAnswerSelect(index)}
+          >
+            <input
+              type="radio"
+              id={`question-${questionIndex}-answer-${index}`}
+              name={`question-${questionIndex}`}
+              value={index}
+              checked={userAnswer === index}
+              onChange={() => handleAnswerSelect(index)}
+            />
+            <label htmlFor={`question-${questionIndex}-answer-${index}`}>{answer}</label>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
